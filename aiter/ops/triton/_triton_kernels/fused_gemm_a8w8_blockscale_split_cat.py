2242ba1a1de278b9f32d8496b0872494b080b956
ShaoChunLee <Shao-Chun.Lee@amd.com>
2025-12-15 14:05:00 +0000

    fix get_arch

--- aiter/ops/triton/_triton_kernels/fused_gemm_a8w8_blockscale_split_cat.py
+++ aiter/ops/triton/_triton_kernels/fused_gemm_a8w8_blockscale_split_cat.py
@@ -380,7 +380,7 @@ def _get_config(
     K: int,
 ) -> dict:
     if not hasattr(_get_config, "_config_dict"):
-        dev = arch_info.get_device()
+        dev = arch_info.get_arch()
         _get_config._config_dict = {}
         fpath = f"{AITER_TRITON_CONFIGS_PATH}/gemm/{dev}-GEMM-A8W8_BLOCKSCALE.json"
         with open(fpath, "r") as file:
@@ -389,7 +389,7 @@ def _get_config(
 
     key = f"{N}_{K}"
     if key not in _get_config._config_dict.keys():
-        dev = arch_info.get_device()
+        dev = arch_info.get_arch()
         fpath = f"{AITER_TRITON_CONFIGS_PATH}/gemm/{dev}-GEMM-A8W8_BLOCKSCALE-N={N}-K={K}.json"
         if os.path.exists(fpath):
             with open(fpath, "r") as file:
